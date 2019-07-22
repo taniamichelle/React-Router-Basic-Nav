5d2e195bc28284dadb4244c146c8e97168e7e1be
tania <tanislavens@yahoo.com>
2019-07-22 16:50:14 -0400

    wip: setting up App js file

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Route} from 'react-router-dom';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
 
@@ -6,6 +7,9 @@ const App = () => (
   <div>
     <Navigation />
   </div>
+  <Route exact path= '/' component={Home} />
+  <Route exact path='/about' component={About} />
+  <Route exact path='/contact' component={Contact} />
 );
 
 export default App;
